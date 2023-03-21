99348e74aa1cc89fa08c26fad951fa4ec5497ba9
Sarah Abou Dalal <sarahaboudalal@gmail.com>
2023-03-21 14:27:27 +0200

    adjusted folder structure

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,8 +4,8 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AppHomeComponent } from './app-home/app-home.component';
-import { MagnifierDirective } from './magnifier.directive';
-import { YearPipe } from './year.pipe';
+import { MagnifierDirective } from './directives/magnifier.directive';
+import { YearPipe } from './pipes/year.pipe';
 
 @NgModule({
   declarations: [AppComponent, AppHomeComponent, MagnifierDirective, YearPipe],
--- src/app/magnifier.directive.spec.ts
+++
@@ -1,8 +0,0 @@
-import { MagnifierDirective } from './magnifier.directive';
-
-describe('MagnifierDirective', () => {
-  it('should create an instance', () => {
-    const directive = new MagnifierDirective();
-    expect(directive).toBeTruthy();
-  });
-});
